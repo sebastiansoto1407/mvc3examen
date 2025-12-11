f2778d3305199e01438cbd368060cd8c346ebc06
sebastiansoto1407 <sebastiansotoastete1407@gmail.com>
2025-12-11 18:23:40 -0400

    guardadomvcsebastiansoto

---
+++ examen3parcialmvc/examen3parcialmvc/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace examen3parcialmvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
